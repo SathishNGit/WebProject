38116582ae5272d862cb1c5280bf7d018a2ac91f
SathishNGit <43092687+SathishNGit@users.noreply.github.com>
2019-09-06 14:47:07 +0530

    Add files via upload

--- jenkins_test.java
+++ jenkins_test.java
@@ -1,6 +1,7 @@
 
 public class jenkins_test {
+	static String msg = "Hi";
 	  public static void main(String[] args) {
-	    System.out.println("Hello, World");
+	    System.out.println("Hello, World...." + msg);
 	  }
 	}
